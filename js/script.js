3508cd3d0762dffba481a1f9a2e1ded674b98e13
Shubham Singh <shubham.singh12@infosys.com>
2019-10-09 14:40:54 +0530

    Javascript functions added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-$(window).on("load",function(){
-    $(".loader .inner").fadeOut(500,function(){
-        $(".loader").fadeOut(750);
-    });
+$(window).on("load", function () {
+	$(".loader-wrapper").fadeOut(500, function () {
+	});
 
-    $(".items").isotope({
+	$(".items").isotope({
 		filter: '*',
 		animationOptions: {
 			duration: 1500,
@@ -15,7 +14,7 @@ $(window).on("load",function(){
 });
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 	$('#slides').superslides({
 		animation: 'fade',
@@ -28,68 +27,67 @@ $(document).ready(function() {
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
-    showCursor: false,
-  });
-  
-  var skillInfo = new Typed(".skillInfo", {
-		strings: ["Scroll to see more skills..."],
-    typeSpeed: 50,
-    loop: true,
-    startDelay: 1000,
-    showCursor: false,
-    fadeOut: true
+		showCursor: false,
+	});
+
+	var skillInfo = new Typed(".skillInfo", {
+		strings: ["Drag to see more skills.."],
+		typeSpeed: 50,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false,
 
 
-  })
+	})
 
 	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	            items:4
-	        }
-	    }
+		loop: true,
+		items: 4,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			938: {
+				items: 4
+			}
+		}
 	});
 
 
-	
+
 
 
 	var skillsTopOffset = $(".skillsSection").offset().top;
 	var statsTopOffset = $(".statsSection").offset().top;
 	var countUpFinished = false;
-	$(window).scroll(function() {
+	$(window).scroll(function () {
 
-		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+		if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 
 			$('.chart').easyPieChart({
-		        easing: 'easeInOut',
-		        barColor: '#fff',
-		        trackColor: false,
-		        scaleColor: false,
-		        lineWidth: 4,
-		        size: 152,
-		        onStep: function(from, to, percent) {
-		        	$(this.el).find('.percent').text(Math.round(percent));
-		        }
-		    });
+				easing: 'easeInOut',
+				barColor: '#fff',
+				trackColor: false,
+				scaleColor: false,
+				lineWidth: 4,
+				size: 152,
+				onStep: function (from, to, percent) {
+					$(this.el).find('.percent').text(Math.round(percent));
+				}
+			});
 
 
 		}
 
 
-		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-			$(".counter").each(function() {
+		if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function () {
 				var element = $(this);
 				var endVal = parseInt(element.text());
 
@@ -107,7 +105,7 @@ $(document).ready(function() {
 	$("[data-fancybox]").fancybox();
 
 
-	$("#filters a").click(function() {
+	$("#filters a").click(function () {
 
 		$("#filters .current").removeClass("current");
 		$(this).addClass("current");
@@ -124,45 +122,45 @@ $(document).ready(function() {
 		});
 
 		return false;
-    });
+	});
 
 
-	$("#navigation .brand a").click(function(e){
+	$("#navigation .brand a").click(function (e) {
 		e.preventDefault();
 		var targetElement = $(this).attr("href");
 		var targetPosition = $(targetElement).offset().top;
-        $("html,body").animate({scrollTop: 0},"slow");
-    });
-
-    $("#navigation li a").click(function(e){
-        e.preventDefault();
-		var targetElement = $(this).attr("href");
-		var targetPosition = $(targetElement).offset().top;
-        $("html,body").animate({scrollTop: targetPosition -50},"slow");
+		$("html,body").animate({ scrollTop: 0 }, "slow");
 	});
 
-		
- 
-    const nav = $("#navigation");
-    const navTop = nav.offset().top;
+	$("#navigation li a").click(function (e) {
+		e.preventDefault();
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html,body").animate({ scrollTop: targetPosition - 50 }, "slow");
+	});
 
-	$(window).on("scroll",stickNavigation);
-    $(window).on("scroll",enterSection);
-	
-    
-    function stickNavigation(){
-        var body = $("body");
 
-        if($(window).scrollTop() >= navTop){
-            body.css("padding-top",nav.outerHeight()+ "px");
-            body.addClass("fixedNav");
 
-        } else{
-            body.css("padding-top",0);
-            body.removeClass("fixedNav");
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
 
-        }
-        
+	$(window).on("scroll", stickNavigation);
+	$(window).on("scroll", enterSection);
+
+
+	function stickNavigation() {
+		var body = $("body");
+
+		if ($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+
+		} else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+
+		}
+
 	}
 
 	var current = navTop;
@@ -173,8 +171,8 @@ $(document).ready(function() {
 	const contactTop = $("#contact").offset().top;
 
 
-	function enterSection(){
-		if($(window).scrollTop() < navTop){
+	function enterSection() {
+		if ($(window).scrollTop() < navTop) {
 			$(".brand").addClass("active");
 			$(".item0").removeClass("active");
 			$(".item1").removeClass("active");
@@ -182,7 +180,7 @@ $(document).ready(function() {
 			$(".item3").removeClass("active");
 			$(".item4").removeClass("active");
 		}
-		else if ($(window).scrollTop() <= aboutTop){
+		else if ($(window).scrollTop() <= aboutTop) {
 			$(".item0").addClass("active");
 			$(".brand").removeClass("active");
 			$(".item1").removeClass("active");
@@ -190,7 +188,7 @@ $(document).ready(function() {
 			$(".item3").removeClass("active");
 			$(".item4").removeClass("active");
 		}
-		else if ($(window).scrollTop() > aboutTop + 600 && $(window).scrollTop() < skillsTop  ){
+		else if ($(window).scrollTop() > aboutTop + 600 && $(window).scrollTop() < skillsTop) {
 			$(".item1").addClass("active");
 			$(".brand").removeClass("active");
 			$(".item0").removeClass("active");
@@ -198,7 +196,7 @@ $(document).ready(function() {
 			$(".item3").removeClass("active");
 			$(".item4").removeClass("active");
 		}
-		else if ($(window).scrollTop() > skillsTop + 500 && $(window).scrollTop() < statsTop  ){
+		else if ($(window).scrollTop() > skillsTop + 500 && $(window).scrollTop() < statsTop) {
 			$(".item2").addClass("active");
 			$(".brand").removeClass("active");
 			$(".item1").removeClass("active");
@@ -206,7 +204,7 @@ $(document).ready(function() {
 			$(".item3").removeClass("active");
 			$(".item4").removeClass("active");
 		}
-		else if ($(window).scrollTop() > statsTop + 300 && $(window).scrollTop() < portfolioTop  ){
+		else if ($(window).scrollTop() > statsTop + 300 && $(window).scrollTop() < portfolioTop) {
 			$(".item3").addClass("active");
 			$(".brand").removeClass("active");
 			$(".item1").removeClass("active");
@@ -214,7 +212,7 @@ $(document).ready(function() {
 			$(".item0").removeClass("active");
 			$(".item4").removeClass("active");
 		}
-		else if ($(window).scrollTop() > portfolioTop + 800 && $(window).scrollTop() < contactTop  ){
+		else if ($(window).scrollTop() > portfolioTop + 800 && $(window).scrollTop() < contactTop) {
 			$(".item4").addClass("active");
 			$(".brand").removeClass("active");
 			$(".item1").removeClass("active");
@@ -223,7 +221,7 @@ $(document).ready(function() {
 			$(".item0").removeClass("active");
 		}
 	}
-	
+
 
 
 });
